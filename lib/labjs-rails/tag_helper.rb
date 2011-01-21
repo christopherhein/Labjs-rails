bf496db71eb8b80d4b858c2baf4948f4ebf342cc
Christopher Hein <me@christopherhein.com>
2011-01-21 03:31:07 -0800

    refactored both methods

--- lib/labjs-rails/tag_helper.rb
+++ lib/labjs-rails/tag_helper.rb
@@ -3,32 +3,31 @@ module Labjs
   
   module TagHelper
     
-    def labjs_init(file, dir=DEFAULT_DIR)
+    def labjs_load(file, dir=DEFAULT_DIR)
+      file = labjs_format(file, dir)
+      "<script type=\"text/javascript\" src=\"#{file}\"></script>".html_safe
+    end
+    
+    def labjs_include_tag(files = {}, dir=DEFAULT_DIR)
+      scripts = "<script type=\"text/javascript\">"
+      scripts += "$LAB"
+      files.each do |type, file|
+        file = labjs_format(file, dir)
+        scripts += ".script('"+file+"')"
+        if type =~ /require/
+          scripts += ".wait()"
+        end
+      end
+      scripts += "</script>"
+      scripts.html_safe
+    end
+    
+    def labjs_format(file, dir)
       file = "/#{dir}/#{file}" unless file =~ /\/?#{dir}/
       file.gsub!(/\/+/, '/')
       file = "#{file}.js" unless file =~ /\.js$/
       file = "/#{file}" unless file =~ /^\//
-      "<script type=\"text/javascript\" src=\"#{file}\"></script>".html_safe
-    end
-    
-    def labjs_include_tag(required, files = [], dir=DEFAULT_DIR)
-      scripts = "<script type=\"text/javascript\">"
-      
-      required = "/#{dir}/#{required}" unless required =~ /\/?#{dir}/
-      required.gsub!(/\/+/, '/')
-      required = "#{required}.js" unless required =~ /\.js$/
-      required = "/#{required}" unless required =~ /^\//
-      
-      scripts += "$LAB.script('"+required+"').wait()"
-      files.each do |file|
-        file = "/#{dir}/#{file}" unless file =~ /\/?#{dir}/
-        file.gsub!(/\/+/, '/')
-        file = "#{file}.js" unless file =~ /\.js$/
-        file = "/#{file}" unless file =~ /^\//
-        scripts += ".script('"+file+"')"
-      end
-      scripts += "</script>"
-      scripts.html_safe
+      return file
     end
 
   end
--- test/test_tag_helper.rb
+++ test/test_tag_helper.rb
@@ -7,16 +7,18 @@ class TestTagHelper < Test::Unit::TestCase
   end
   
   should "return initalize LABjs" do
-    assert_equal "<script type=\"text/javascript\" src=\"/javascripts/labjs.js\"></script>", @controller.labjs_init("/javascripts/labjs.js")
-    assert_equal "<script type=\"text/javascript\" src=\"/javascripts/labjs.js\"></script>", @controller.labjs_init("labjs.js")
-    assert_equal "<script type=\"text/javascript\" src=\"/javascripts/labjs.js\"></script>", @controller.labjs_init("javascripts/labjs.js")
-    assert_equal "<script type=\"text/javascript\" src=\"/javascripts/labjs.js\"></script>", @controller.labjs_init("javascripts/labjs")
-    assert_equal "<script type=\"text/javascript\" src=\"/javascripts/labjs.js\"></script>", @controller.labjs_init("labjs")
+    assert_equal "<script type=\"text/javascript\" src=\"/javascripts/labjs.js\"></script>", @controller.labjs_load("/javascripts/labjs.js")
+    assert_equal "<script type=\"text/javascript\" src=\"/javascripts/labjs.js\"></script>", @controller.labjs_load("labjs.js")
+    assert_equal "<script type=\"text/javascript\" src=\"/javascripts/labjs.js\"></script>", @controller.labjs_load("javascripts/labjs.js")
+    assert_equal "<script type=\"text/javascript\" src=\"/javascripts/labjs.js\"></script>", @controller.labjs_load("javascripts/labjs")
+    assert_equal "<script type=\"text/javascript\" src=\"/javascripts/labjs.js\"></script>", @controller.labjs_load("labjs")
   end
 
-  should "return valid script tag" do
-    assert_equal "<script type=\"text/javascript\">$LAB.script('/javascripts/jquery.js').wait()</script>", @controller.labjs_include_tag('jquery')
-    assert_equal "<script type=\"text/javascript\">$LAB.script('/javascripts/jquery.js').wait().script('/javascripts/jquery.ui.js').script('/javascripts/jquery.cookie.js')</script>", @controller.labjs_include_tag('jquery', ['jquery.ui', 'jquery.cookie']),
+  should "return valid script labjs tags" do
+    assert_equal "<script type=\"text/javascript\">$LAB.script('/javascripts/jquery.min.js').wait()</script>", @controller.labjs_include_tag({'require_0' => 'jquery.min'})
+    assert_equal "<script type=\"text/javascript\">$LAB.script('/javascripts/jquery.min.js').wait().script('/javascripts/jquery.ui.js').script('/javascripts/jquery.cookie.js')</script>", @controller.labjs_include_tag({'require_0' => 'jquery.min', 'ui' => 'jquery.ui', 'cookie' => 'jquery.cookie'})
+    assert_equal "<script type=\"text/javascript\">$LAB.script('/javascripts/jquery.min.js').wait().script('/javascripts/jquery.ui.js').wait().script('/javascripts/jquery.cookie.js')</script>", @controller.labjs_include_tag({'require_0' => 'jquery.min','require_1' => 'jquery.ui', 'cookie' => 'jquery.cookie'})
   end
+
 
 end
